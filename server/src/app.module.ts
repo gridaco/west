d84e54fabce4f35e7f1ff3b45e2442153f7b2e32
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-19 20:57:09 +0900

    yarn

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { GithubEventsModule } from "events/github";
 
 @Module({
-  imports: [],
+  imports: [GithubEventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
