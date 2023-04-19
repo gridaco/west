8fdaf43f26c4543cda5abcd389a77d497dbc8a49
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-19 21:44:38 +0900

    add validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,21 +2,36 @@ import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ServerModule } from "./server.module";
 import { PrismaService } from "./services";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
 
+  // initialize prisma
+  await _prisma(app);
+
+  // initialize swagger
+  _swagger(app);
+
+  // enable validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
+}
+
+async function _prisma(app) {
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+}
+
+function _swagger(app) {
   const config = new DocumentBuilder()
     .setTitle("WEST API")
     .setDescription("The WEST API description")
     .setVersion("0")
-    .addTag("west")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/", app, document);
-
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
 }
+
 bootstrap();
