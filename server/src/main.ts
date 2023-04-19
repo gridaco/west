540ff2d42153f0209e06bab572ae61f6d48aaa31
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-19 21:33:04 +0900

    add swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ServerModule } from "./server.module";
 import { PrismaService } from "./services";
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("WEST API")
+    .setDescription("The WEST API description")
+    .setVersion("0")
+    .addTag("west")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/", app, document);
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
