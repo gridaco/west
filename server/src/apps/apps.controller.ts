ed86b6ebabdcbfa366aba5d929b96817ccdcc708
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-19 21:33:29 +0900

    route

--- server/src/apps/apps.controller.ts
+++ server/src/apps/apps.controller.ts
@@ -4,4 +4,9 @@ import { AppsService } from "./apps.service";
 @Controller("/apps")
 export class AppsController {
   constructor(private readonly service: AppsService) {}
+
+  @Post()
+  async new(@Req() request: Request, @Body() body) {
+    //
+  }
 }
